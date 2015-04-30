1e253119a1020d3bd9b89041af30a9bb4e8283fa
Steven Brice <brice.steven@gmail.com>
2015-04-30 15:21:06 -0700

    Added Blank Assignment 5

---
+++ A5-Download-Images/A5-Download-Images/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  A5-Download-Images
+//
+//  Created by Steven Brice  on 4/29/15.
+//  Copyright (c) 2015 Steven Brice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
